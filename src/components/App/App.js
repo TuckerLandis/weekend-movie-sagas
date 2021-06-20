ac1153d5ec93946b02953ab2bc16738f2bd6d0cc
TuckerLandis <landistuckerc@gmail.com>
2021-06-20 01:43:25 -0500

    card and overal theme

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,13 +4,31 @@ import MovieList from '../MovieList/MovieList'
 import MovieForm from '../MovieForm/MovieForm';
 import Details from '../Details/Details'
 import Nav from '../Nav/Nav'
+import { CssBaseline } from '@material-ui/core';
 
-import { ThemeProvider } from '@material-ui/core/styles';
-import theme from './theme';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#3f51b5',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+    background: {
+      default: '#212121',
+    },
+  },
+  
+});
+
+
 
 function App() {
   return (
     <ThemeProvider theme={theme} >
+      <CssBaseline /> 
     <div className="App">
       <h1>The Movies Saga!</h1>
       
